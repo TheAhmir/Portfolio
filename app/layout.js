c16346e04ba8a9e8f7642c3645d6e0fcbbae5799
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2024-07-08 08:46:56 -0400

    adding analytics

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,17 @@ export default function RootLayout({ children }) {
           sizes="<generated>"
         />
       </head>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}/>
+      <script dangerouslySetInnerHTML={{
+        __html : `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `
+      }}
+      />
       <ClerkProvider appearance={{
         elements: {
           footer: "hidden",
