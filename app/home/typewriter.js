3c8a1fbece2f1cca79a30174e0c3b45ff8a7e51e
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2024-07-09 02:56:01 -0400

    idk

--- app/home/typewriter.js
+++ app/home/typewriter.js
@@ -1,30 +1,30 @@
-'use client'
+'use client';
 
 import React, { useEffect, useState } from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import './intro_components.css';
 
 export default function Typewriter_Component() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
-    const [typewriterText] = useTypewriter({
-        words: [
-            "William and Mary, 2024 Graduate",
-            "Data Analyst",
-            "Website developed using React"
-        ],
-        loop: 1,
-        delaySpeed: 2000,
-    });
+  const [typewriterText] = useTypewriter({
+    words: [
+      "William and Mary, 2024 Graduate",
+      "Data Analyst",
+      "Website developed using React"
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
-    useEffect(() => {
-        setText(typewriterText);
-    }, [typewriterText]);
+  useEffect(() => {
+    setText(typewriterText);
+  }, [typewriterText]);
 
-    return (
-        <h3>
-            <span className='job-title'>{text}</span>
-            <Cursor cursorColor='#313bac' />
-        </h3>
-    );
+  return (
+    <h3>
+      <span className='job-title'>{text}</span>
+      <Cursor cursorColor='#313bac' />
+    </h3>
+  );
 }
