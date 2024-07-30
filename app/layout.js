a6406113a499ab0af73a1a3a2688297a33f19819
AhmirPostell <124746123+AhmirPostell@users.noreply.github.com>
2024-07-29 21:35:42 -0400

    threading the needle

--- app/layout.js
+++ app/layout.js
@@ -37,11 +37,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link
-          rel="canonical"
-          href="https://ahmirpostell.com"
-          key="canonical"
-        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
           rel="icon"
