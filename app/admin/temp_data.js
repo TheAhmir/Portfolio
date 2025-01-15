c1e6bd6d55b73e5b1156d4651d4d38616a16e9af
Ahmir Postell <postell.ahmir@gmail.com>
2025-01-15 00:59:30 -0500

    removed admin

--- app/admin/temp_data.js
+++
@@ -1,9 +0,0 @@
-export const folderIDS = [
-    '831e6726-77e1-4fa2-849f-b407ee3c616d',
-    '0ff59d84-0e9b-443d-8ba9-eea27c95568d',
-    'ddb06923-a829-48a9-9f93-09e89a86dc0f'
-]
-
-export const noteIDS = [
-    '452351c2-446a-4d05-b270-3734eeac50eb'
-]
